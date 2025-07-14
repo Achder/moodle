d0b2f0f98298d762b38f4d75b83ea095a19f1a9b
Thomas Meerpohl <thomas.meerpohl@gmail.com>
2025-07-15 00:18:23 +0200

    Added styles and fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
@@ -7,4 +7,13 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Moirai One",
+        cssVariable: "--font-moodle"
+      }
+    ]
+  }
 });
