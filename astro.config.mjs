121bf6320a9469df7fce029ba1c493d122d619cc
Thomas Meerpohl <thomas.meerpohl@gmail.com>
2025-07-15 08:08:21 +0200

    Adjusted styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { qrcode } from 'vite-plugin-qrcode';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), qrcode()],
   },
   experimental: {
     fonts: [
